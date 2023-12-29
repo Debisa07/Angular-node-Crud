3a586c5d8f9c4fee8f9deea7a3eab2770b02265c
Debisa07 <debisaabebesa@gmail.com>
2023-12-29 22:40:13 +0300

    mongo connection

--- Server/index.js
+++ Server/index.js
@@ -13,3 +13,18 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log("Server running on port: ", PORT);
 });
+
+
+
+const mongoose = require("mongoose");
+mongoose.
+    connect("mongodb+srv://tinsaebirhan7:D9EubbEi5lJpDCW1@cluster0.ovqcrvw.mongodb.net/ReserveXP?retryWrites=true&w=majority")
+    .then(() => {
+        app.getMaxListeners('/blog', (req, res) => {
+            res.send("Hello Node API")
+
+        })
+        console.log("connected to mongo")
+    }).catch((error) => {
+        console.log(error)
+    })
